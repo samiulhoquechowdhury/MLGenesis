f9414156bc2b560e66dc9c440ff9d26006c98c4c
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-24 01:48:38 +0530

    variable

---
+++ Python/Fundamentals/02-variable.py
@@ -0,0 +1,10 @@
+# A variable is a name given to a memory location
+
+# name = "Samiul"  here name is the variabe and "samiul" is value.
+
+name = "samiul"
+age = 23
+price = 25.99
+
+print(name)
+print("My name is ", name ," and my age is ", age+1)
