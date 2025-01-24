8339ad0099a73374523e52048d58f8bee1513d0f
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-24 12:51:46 +0530

    operators in python

---
+++ Python/Fundamentals/04-Operators.py
@@ -0,0 +1,51 @@
+# types of operator
+
+#Arithmetic operator
+a = 5
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b) #Division (float)
+print(a % b) #Modulus (Rreminder)
+print(a ** b) #Exponential a ** b ==> 625
+
+
+
+#Relational Operator/ Comparision Operator
+# (   == , != , > , < , <= , >=   )
+
+
+c = 7 
+d = 8
+print(c == d)  #False
+print(c != d)  #True
+print(c > d)   #False
+print(c < d)   #Ture
+print(c <= d)  #True
+print(c >= d)  #False
+
+
+
+#Assignment Operator
+# (   = , += , -= , *= , /= , %= , **=   )
+num = 10
+num = num + 10   #10 + 10 = 20
+print("Num : ",num)
+
+
+
+
+#Logical Operator (not, and, or)
+
+e = 50
+f = 30
+print(not False)
+print(not (e > f))
+
+val1 = True
+val2 = False
+
+print("AND operator: ", val1 and val2)
+print("OR operator: ", val1 or val2)
