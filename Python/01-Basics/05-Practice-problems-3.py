3cc10a4cbdec51b7051e2423715ff30ed9a9ea15
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-24 13:09:02 +0530

    Practice problem 3

---
+++ Python/01-Basics/05-Practice-problems-3.py
@@ -0,0 +1,6 @@
+#write a program ti input 2 floating points number and print their average.
+
+first = float(input("Enter first:"))
+second = float(input("Enter second:"))
+
+print("Average :",(first+second)/2)
--- Python/01-Basics/tempCodeRunnerFile.py
+++ Python/01-Basics/tempCodeRunnerFile.py
@@ -1,6 +1,6 @@
 #write a program ti input sides of a square and print its area.
 
 
-side = float(input("Enter the sides of the square:"))
+side = int(input("Enter the sides of the square:"))
 
 print("Area:", side*side)
