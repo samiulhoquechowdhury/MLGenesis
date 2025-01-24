576ec68d124cca2624b56ab89e37a65e5c7b2f4f
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-24 13:04:35 +0530

    practice problems

---
+++ Python/01-Basics/05-Practice-problem-1.py
@@ -0,0 +1,9 @@
+#Write a program to input two numbers and print their sums
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+sum = num1 + num2
+print("The sum of num1 and num2 is",sum)
+
+
---
+++ Python/01-Basics/05-Practice-problem-2.py
@@ -0,0 +1,6 @@
+#write a program ti input sides of a square and print its area.
+
+
+sides = int(input("Enter the sides of the square:"))
+
+print("Area:", sides**2)
