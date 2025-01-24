8b6bbd82c84576c7fb85e742bcbb1f73e349d43d
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-24 13:20:09 +0530

    practice problem

---
+++ Python/01-Basics/05-Practice-problems-4.py
@@ -0,0 +1,7 @@
+#WAP to input two int number a and b Print True if a is greater than or equal to b. if not print false.
+
+
+a = int(input("Enter first :"))
+b = int(input("Enter second :"))
+
+print(a>=b)
