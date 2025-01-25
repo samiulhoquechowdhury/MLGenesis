2a3540f46eb7a058e5cebe948cabca0d33f0632e
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-25 22:32:23 +0530

    Lists in python

---
+++ Python/03-List and tuple/01-List.py
@@ -0,0 +1,11 @@
+#List is a build in datatype that store set of values
+#It can store element of different types(intezer, float, string, etc.)
+
+
+marks = [78, 65, 33, 95, 90]
+student = ["karan", 85, "Delhi"]
+
+print(marks[1])
+
+print(marks.sort(reverse=True))
+print(marks)
